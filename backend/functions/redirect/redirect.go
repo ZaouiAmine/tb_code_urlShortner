f533e91fc24d49a2144ed96f52f84448d0e6cf7b
ZaouiAmine <dev.zaoui@gmail.com>
2025-07-01 14:57:32 +0100

    just a test commit

--- backend/functions/redirect/redirect.go
+++ backend/functions/redirect/redirect.go
@@ -2,9 +2,11 @@ package lib
 
 import (
 	"github.com/taubyte/go-sdk/event"
+	"net/http"
 )
 
 //export redirect
 func redirect(e event.Event) uint32 {
+	http.Redirect(w, r, "https://google.com", http.StatusFound)
 	return 0
 }
